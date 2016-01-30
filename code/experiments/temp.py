3e3a955af04befcd7bfa50b76429f2632639e3e6
Charles Garfinkle <cjfinks@gmail.com>
2016-01-30 15:29:51 -0800

    discarded useless files

--- code/experiments/temp.py
+++
@@ -1,7 +0,0 @@
-"""
-from utils import Arrow3D
-for vec in Ahat.T:
-	arrow = Arrow3D([0,vec[0]],[0,vec[1]],[0,vec[2]], mutation_scale=20, lw=1, arrowstyle="-|>", color="r")
-	ax.add_artist(arrow)
-pp.draw()
-"""
